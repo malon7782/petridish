3e3ada3a12e24018214d9ec566e7b97a9b45e94f
cirnovsky <pup@guanyu.me>
2026-08-28 23:22:32 +0800

    refactor: squash event.go and log.go into one file
    as per malon's comment.

--- event.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type Event struct {
-	Day int
-	Msg string
-}
--- log.go
+++ log.go
@@ -1,5 +1,10 @@
 package main
 
+type Event struct {
+	Day int
+	Msg string
+}
+
 type Logger struct {
 	Events []Event
 	Num    int
