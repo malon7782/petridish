fc0907ad9661e3865feea0fa1b2320eb5015b639
Tian Yuchen <cat@malon.dev>
2026-09-06 01:06:10 +0800

    deco: 雷德王还有三小时降临地球
    when it rains too heavily, it may start to thunder.
    
    purely visual effect - which means the corresponding logic won't write
    in w.Weathers but only read it.
    
    in short, we detect the remaining days of raining and randomly choose a
    day to start lightning if then rain intensity is not too small. whether
    is day is chosen as well as the remaining day of thundering are instead
    stored in deco.go itself, acting like static variables. then we start
    to modified the gradiently change the colors of every element in Grid
    to white~grey...
    
    NOTE: it's very likely that one day lightning will become not only a
    visual effect but also a real type of weather. so it will possibly
    be migrated to weather.go, inside SimulateWeather(), then. but for now,
    let's keep it as it is.

--- deco.go
+++ deco.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func (w *World) decorateGrid(g [][]Cell) {
 	w.decoRain(g, w.Weathers.RainIntensity)
+	w.decoLightning(g, w.Weathers.RainIntensity)
 }
 
 func (w *World) decoRain(g [][]Cell, Intensity float64) {
@@ -15,4 +16,38 @@ func (w *World) decoRain(g [][]Cell, Intensity float64) {
 			Color: fmt.Sprintf("\033[38;5;%dm", 195),
 		}
 	}
+}
+
+// lightning decoration
+
+var (
+	lightningDays   int
+	lightningRolled bool
+)
+
+func (w *World) decoLightning(g [][]Cell, Intensity float64) {
+	if lightningRolled == false && w.Weathers.RainLeft >= 4 &&
+		w.Weathers.RainIntensity > 0.4 {
+		if roll := w.Rng.Intn(10); roll > 5 {
+			lightningDays = 3
+			lightningRolled = true
+		}
+	}
+
+	if lightningRolled == true {
+		if lightningDays == 0 {
+			lightningRolled = false
+			lightningDays = 0
+			return
+		}
+		v := 255 - int(float64(3-lightningDays)/2*(255-64))
+		c := fmt.Sprintf("\033[38;2;%d;%d;%dm", v, v, v)
+		for y := range g {
+			for x := range g[y] {
+				g[y][x].Color = c
+			}
+		}
+		lightningDays--
+	}
+
 }
